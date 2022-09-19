7c25a7d785a73a02bcdb0b2da81eec10db3a31ec
Collen4 <collenleotlela@gmail.com>
2022-09-19 10:12:11 -0700

    adding the 0-reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the pointer to a parameter.
+ * @ptr: input integer.
+ * Return: no return.
+ */
+void reset_to_98(int *ptr)
+{
+	*ptr = 98;
+}
