edda1ba0ae736b9c48665b0806abc1839014651f
Collen4 <collenleotlela@gmail.com>
2022-09-19 10:57:18 -0700

    adding the files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of pointer from parameter.
+ * @ptr: input integer.
+ * Return: no return.
+ */
+void reset_to_98(int *ptr)
+{
+	*ptr = 98;
+}
