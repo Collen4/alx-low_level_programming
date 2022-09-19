d2b9a8ec5857f8578459932dc889caafb604f9fd
Collen4 <collenleotlela@gmail.com>
2022-09-19 10:16:44 -0700

    adding files to repository

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void swap_int)int *a, int *b);
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
