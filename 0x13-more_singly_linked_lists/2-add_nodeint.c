2ea3835f0a675ca3ec5c568c65a7c66354f5b1fa
Collen4 <collenleotlela@gmail.com>
2022-10-24 22:02:56 -0700

    adding changes to task 2.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
  * @n: n elements.
  * Return: address of the new elements, NUll if it failed.
  */
-listint_s *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
